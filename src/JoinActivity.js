87583355ecf7d66b45f1489cc14bba5d3c5e76d1
pegasis <admin@pegasis.site>
2020-01-14 00:53:01 -0500

    add join game screen

---
+++ src/JoinActivity.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./JoinActivity.css"
+import logo from "./pegahoot_logo.png"
+
+export function JoinActivity(props) {
+    return <div className={"full-page"}>
+        <img className={"logo"} src={logo} alt="Logo"/>
+        <p className={"join-text"}>Join at <u>hoot.pegasis.site</u></p>
+        <div className={"username-container"}>
+            {props.users.map((user) => <UserCard user={user} key={user}/>)}
+        </div>
+        {props.users.length>0?<button className={"start-btn"} onClick={props.startGame}>Start</button>:null}
+    </div>
+}
+
+function UserCard(props) {
+    let user = props.user
+    return <div className={"user-card"}>
+        {user}
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {Component} from "react"
+import React from "react"
+import ReactDOM from "react-dom"
+import {JoinActivity} from "./JoinActivity"
+import "./index.css"
 
-class App extends React.Component {
+const wsUrl = "ws://localhost:8080/display"
+
+class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            "activity": "JOIN",
+            "users":[]
         }
+        this.socket = new WebSocket(wsUrl)
+        let self = this
+
+        this.socket.onopen = function (event) {
+            console.log("Socket connected")
+            // socket.send("My name is John");
+        }
+
+        this.socket.onmessage = function (event) {
+            console.log(`Socket received: ${event.data}`)
+            self.setState(JSON.parse(event.data))
+        }
+
+        this.socket.onclose = function (event) {
+            if (event.wasClean) {
+                console.log(`Socket closed cleanly, code=${event.code} reason=${event.reason}`)
+            } else {
+                console.log('Socket died')
+            }
+        }
+
+        this.socket.onerror = function (error) {
+            alert(`Socket error: ${error.message}`)
+        }
+
+        this.startGame = this.startGame.bind(this)
+    }
+
+    startGame(){
+        console.log("start game")
     }
 
     render() {
+        if (this.state.activity==="JOIN"){
+            return <JoinActivity users={this.state.users} startGame={this.startGame}/>
+        }
         return <div/>
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'))
