b164e8e359bd9b39d0cead25f7a51b17b044c81d
pegasis <admin@pegasis.site>
2020-01-14 02:10:24 -0500

    small changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import {Component} from "react"
-import React from "react"
+import React, {Component} from "react"
 import ReactDOM from "react-dom"
 import {JoinActivity} from "./JoinActivity"
 import "./index.css"
